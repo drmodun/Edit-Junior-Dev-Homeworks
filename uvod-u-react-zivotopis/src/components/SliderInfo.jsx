3f35bf7cf8db6fe44cb3d70b197b84e983de19bc
drmodun <jan.modun.st@gmail.com>
2023-03-29 00:41:10 +0200

    formatted documents

--- uvod-u-react-zivotopis/src/components/SliderInfo.jsx
+++ uvod-u-react-zivotopis/src/components/SliderInfo.jsx
@@ -1,9 +1,9 @@
-export const SliderInfo= (props) =>{
+export const SliderInfo = (props) => {
     return (
         <div className="slider-info">
             <h2>{props.title}</h2>
             <div className="slider">
-                <div className="slider-line" style={{width : String(props.percentage) + "%"}}>{props.percentage}</div>
+                <div className="slider-line" style={{ width: String(props.percentage) + "%" }}>{props.percentage}</div>
             </div>
         </div>
     );
--- uvod-u-react-zivotopis/src/components/TextInfo.jsx
+++ uvod-u-react-zivotopis/src/components/TextInfo.jsx
@@ -1,12 +1,6 @@
 import { useEffect } from "react";
 
 export const TextInfo = (props) => {
-    useEffect(()=>{
-        console.log("TextInfo component mounted");
-        return () => {
-            console.log("TextInfo component unmounted");
-        }
-    } , [])
     return (
         <div className="text-info">
             <h2 className="category">{props.category}:</h2>
